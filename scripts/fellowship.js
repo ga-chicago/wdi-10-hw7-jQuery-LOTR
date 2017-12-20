beaff9d1ea769fd8f6d7662ffbc03f23c9d681c9
Anthony <anthonyjlower@gmail.com>
2017-12-19 22:32:48 -0600

    Final edits and adjusting indentation

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -21,28 +21,33 @@ var body = document.body;
 var section = '<section></section>';
 
 function makeMiddleEarth() {
-   // create a section tag with an id of `middle-earth`
-    let $newSection = $('<section>').attr("id",'middle-earth');
-   // add each land as an `article` tag
-    for(let i = 0; i < lands.length; i++) {
-      let $newArticle = $('<article>').addClass(lands[i]);
-      let $newH1 = $('<h1>').text(lands[i]);
-      $newArticle.append($newH1);
-      $newSection.append($newArticle)
-    }
-   // inside each `article` tag include an `h1` with the name of the land
-    $('body').append($newSection);
-   // append `middle-earth` to your document `body`
+  
+  // create a section tag with an id of `middle-earth`
+  let $newSection = $('<section>').attr("id",'middle-earth');
+  
+  // add each land as an `article` tag
+  for(let i = 0; i < lands.length; i++) {
+
+    // inside each `article` tag include an `h1` with the name of the land
+    let $newArticle = $('<article>').addClass(lands[i]);
+    let $newH1 = $('<h1>').text(lands[i]);
+    $newArticle.append($newH1);
+    $newSection.append($newArticle)
+  }
+
+  // append `middle-earth` to your document `body`
+  $('body').append($newSection);
 }
 
 makeMiddleEarth();
 
 function makeHobbits(){
+
   // display an `unordered list` of hobbits in the shire
-   // (which is the second article tag on the page)
+  // (which is the second article tag on the page)
   // give each hobbit a class of `hobbit`
   let $newList = $('<ul>');
-  
+
   // console.log($newList)
   for(let i = 0; i < hobbits.length; i++){
     let $newItem = $('<li>').addClass('hobbit').text(hobbits[i])
@@ -54,37 +59,44 @@ function makeHobbits(){
 makeHobbits();
 
 function keepItSecretKeepItSafe(){
+  
   // create a div with an id of `'the-ring'`
-    let $newDiv = $('<div>').attr("id", "the-ring");
-    // console.log($newDiv)
-   // give the div a class of `'magic-imbued-jewelry'`
-    $newDiv.addClass('magic-imbued-jewelry');
-    // console.log($newDiv)
-   // add the ring as a child of `Frodo`
-   let frodo = $('li')[0];
-   $(frodo).append($newDiv)
+  let $newDiv = $('<div>').attr("id", "the-ring");
+  // console.log($newDiv)
+  
+  // give the div a class of `'magic-imbued-jewelry'`
+  $newDiv.addClass('magic-imbued-jewelry');
+  // console.log($newDiv)
+
+  // add the ring as a child of `Frodo`
+  let frodo = $('li')[0];
+  $(frodo).append($newDiv)
 }
 
 keepItSecretKeepItSafe();
 
 function makeBuddies(){
-   // create an `aside` tag
-    let $newAside = $('<aside>');
-   // attach an `unordered list` of the `'buddies'` in the aside
-    let $newList = $("<ul>")
-    
-    for(let i = 0; i < buddies.length; i++){
-      let $newItem = $('<li>').text(buddies[i])
-      $newList.append($newItem)
-    }
-   // insert your aside as a child element of `rivendell`
-   $newAside.append($newList);
-   $('.Rivendell').append($newAside)
+  
+  // create an `aside` tag
+  let $newAside = $('<aside>');
+
+  // attach an `unordered list` of the `'buddies'` in the aside
+  let $newList = $("<ul>")
+
+  for(let i = 0; i < buddies.length; i++){
+    let $newItem = $('<li>').text(buddies[i])
+    $newList.append($newItem)
+  }
+
+  // insert your aside as a child element of `rivendell`
+  $newAside.append($newList);
+  $('.Rivendell').append($newAside)
 }
 
 makeBuddies();
 
 function beautifulStranger(){
+
   // change the `'Strider'` text to `'Aragorn'`
   let aragorn = $('li')[7];
   $(aragorn).text("Aragorn");
@@ -93,7 +105,8 @@ function beautifulStranger(){
 beautifulStranger();
 
 function leaveTheShire(){
-  // your answers here
+
+   // assemble the `hobbits` and move them to `rivendell`
   let $hobbitList = $('article.The.Shire li')
   $('.Rivendell').append($hobbitList)
 }
@@ -101,32 +114,38 @@ function leaveTheShire(){
 leaveTheShire();
 
 function forgeTheFellowship() {
+
   // create a new div called `'the-fellowship'` within `rivendell`
   let $newDiv = $('<div>').addClass('the-fellowship')
   $('Rivendell').append($newDiv)
+
   // add each `hobbit` and `buddy` one at a time to `'the-fellowship'`
   let newArray = $('li')
   for(let i = 0; i < newArray.length; i++){
     let newItem = newArray[i]
     $('.the-fellowship').append($(newItem));
+
+    // after each character is added make an alert that they // have joined your party
     console.log($(newItem).text() + " has joined your party");
   }
-  // after each character is added make an alert that they // have joined your party
 }
 
 forgeTheFellowship();
 
 function theBalrog(){
+
   // change the `'Gandalf'` text to `'Gandalf the White'`
-    let gandalf = $('li')[0]
-    $(gandalf).text('Gandalf the white')
-   // apply the following style to the element, make the // background 'white', add a grey border
-   $(gandalf).css("background-color", "white").css("border", "2px grey solid")
+  let gandalf = $('li')[0]
+  $(gandalf).text('Gandalf the white')
+
+  // apply the following style to the element, make the // background 'white', add a grey border
+  $(gandalf).css("background-color", "white").css("border", "2px grey solid")
 }
 
 theBalrog();
 
 function hornOfGondor() {
+  
   // pop up an alert that the horn of gondor has been blown
   console.log("The horn of Gondor has been blown")
   // Boromir's been killed by the Uruk-hai!
@@ -155,22 +174,26 @@ function itsDangerousToGoAlone() {
 itsDangerousToGoAlone();
 
 function weWantsIt() {
+
   // Create a div with an id of `'gollum'` and add it to Mordor
-    let $newDiv = $('<div>').attr('id', 'gollum');
-    $('.Mordor').append($newDiv);
-   // Remove `the ring` from `Frodo` and give it to `Gollum`
-    $('.magic-imbued-jewelry').appendTo($('#gollum'));
-   // Move Gollum into Mount Doom
-    $('#mount-doom').append($('#gollum'))
+  let $newDiv = $('<div>').attr('id', 'gollum');
+  $('.Mordor').append($newDiv);
+
+  // Remove `the ring` from `Frodo` and give it to `Gollum`
+  $('.magic-imbued-jewelry').appendTo($('#gollum'));
+  
+  // Move Gollum into Mount Doom
+  $('#mount-doom').append($('#gollum'))
 }
 
 weWantsIt();
 
 function thereAndBackAgain(){
+
   // remove `Gollum` and `the Ring` from the document
   $('#gollum').remove()
-   // Move all the `hobbits` back to `the shire`
-   $('.hobbit').appendTo($(".The.Shire"))
+  // Move all the `hobbits` back to `the shire`
+  $('.hobbit').appendTo($(".The.Shire"))
 }
 
 thereAndBackAgain();
